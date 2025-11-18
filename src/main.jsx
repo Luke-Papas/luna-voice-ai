f91745099ece2b2f6d84cab6c40e2451896b0a7e
Luke-Papas <loucaspapaspyrou@gmail.com>
2025-11-19 00:20:49 +0200

    Rename component from LUNA to ENA

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LUNA from "./App.jsx";
+import ENA from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LUNA />
+    <ENA />
   </React.StrictMode>
 );
 
